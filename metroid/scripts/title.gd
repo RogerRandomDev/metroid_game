f1b38144e2e0121a68be2aa488f116d5f4ebfefb
Roger <rgrang816@west-mec.org>
2022-02-10 15:41:02 -0700

    working on title screen

--- metroid/scripts/title.gd
+++ metroid/scripts/title.gd
@@ -11,5 +11,8 @@ func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/main_game.tscn")
 
 
+
 func _on_settings_pressed():
-	pass # Replace with function body.
+	$Tween.interpolate_property($main_page,"rect_position",Vector2.ZERO,Vector2(0,616),0.5,Tween.TRANS_CUBIC)
+	$Tween.interpolate_property($settings_page,"rect_position",Vector2(0,616),Vector2.ZERO,0.5,Tween.TRANS_CUBIC)
+	$Tween.start()
